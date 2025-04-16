90421456da0a7f5e6d88f6de7d03381bf182e587
Ulenar of Mondarth <ulenarofmondarth@gmail.com>
2025-04-16 20:10:16 +0000

    docs: brief overview

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'EMMA'
+project = 'PJE Preprocess'
 copyright = '2025, Ulenar of Mondarth'
 author = 'Ulenar of Mondarth'
 release = '0.1.0'
