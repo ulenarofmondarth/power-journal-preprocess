54c5b43c8f15d5ec57ebcf73b45ae6790320fb48
Ulenar of Mondarth <ulenarofmondarth@gmail.com>
2025-04-19 15:53:31 +0000

    style: format

--- src/lib/internalMacros/if.ts
+++ src/lib/internalMacros/if.ts
@@ -4,7 +4,7 @@ import type { MacroResult } from '@lib/standardMacroLookup';
 
 export default function (condition: string, truthyValue: string, falsyValue: string = ''): MacroResult {
   try {
-    return { result: eval?.(`"use strict";(${condition})`) ? truthyValue : falsyValue, errors:[] };
+    return { result: eval?.(`"use strict";(${condition})`) ? truthyValue : falsyValue, errors: [] };
   } catch (e) {
     console.error(`ifMacro condition error: ${e}`);
     return {
